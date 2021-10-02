8da0150b03d8da1820ee11d6c973f1b912fda0d1
gnsunayana <gnsunayana12@gmail.com>
2021-10-03 01:02:26 +0530

    initial commit

--- starter/critter/src/main/java/com/udacity/jdnd/course3/critter/schedule/ScheduleDTO.java
+++ starter/critter/src/main/java/com/udacity/jdnd/course3/critter/schedule/ScheduleDTO.java
@@ -1,6 +1,7 @@
 package com.udacity.jdnd.course3.critter.schedule;
 
 import com.udacity.jdnd.course3.critter.user.EmployeeSkill;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -10,6 +11,7 @@ import java.util.Set;
  * Represents the form that schedule request and response data takes. Does not map
  * to the database directly.
  */
+@NoArgsConstructor
 public class ScheduleDTO {
     private long id;
     private List<Long> employeeIds;
@@ -17,6 +19,12 @@ public class ScheduleDTO {
     private LocalDate date;
     private Set<EmployeeSkill> activities;
 
+    public long getId(){
+        return id;
+    }
+    public void setId(long id){
+        this.id = id;
+    }
     public List<Long> getEmployeeIds() {
         return employeeIds;
     }
